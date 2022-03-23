f0070b55fdcbe8f2b5c8482a8afc729990b15281
mj <c00161818@louisiana.edu>
2022-03-23 00:43:01 -0500

    sliding navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { transition, trigger, useAnimation } from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
+import { SidebarOpenAnimation, SidebarCloseAnimation } from './animations';
 
-
+const animationParams = {
+  menuWidth: "250px",
+  animationStyle: "500ms ease"
+};
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger("sideMenu", [
+      transition(":enter", [
+        useAnimation(SidebarOpenAnimation, {
+          params: {
+            ...animationParams
+          }
+        })
+      ]),
+      transition(":leave", [
+        useAnimation(SidebarCloseAnimation, {
+          params: {
+            ...animationParams
+          }
+        })
+      ])
+    ])
+  ]
 })
+
 export class NavBarComponent implements OnInit {
+
+  isOpen = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    this.isOpen = true;
+  }
 }
